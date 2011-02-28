63fa225176e4cb901526b2e35b251871eb855197
Geoff Bache <geoff.bache@jeppesen.com>
2011-02-28 20:38:55 +0100

    Fixing up logging setup

---
+++ log/regenerate_log_config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import logconfiggen, os
+    
+if __name__ == "__main__":
+    gen = logconfiggen.PythonLoggingGenerator("logging.conf", prefix="%(LOCAL_DIR)s/", postfix="diag")
+    enabledLoggerNames = []
+    
+    installationRoot = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    coreLib = os.path.join(installationRoot, "capturemock")
+    coreLoggers = logconfiggen.findLoggerNamesUnder(coreLib)
+        
+    gen.generate(enabledLoggerNames, coreLoggers)
+    
