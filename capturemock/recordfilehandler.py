ef709ae9e252076cccbd8dd2fbbe05b9ed0ef096
Geoff Bache <geoff.bache@jeppesen.com>
2013-10-09 16:09:22 +0200

    tell() is unreliable on Windows, use os.path.getsize instead

--- capturemock/recordfilehandler.py
+++ capturemock/recordfilehandler.py
@@ -1,5 +1,6 @@
 
 """ Very basic interface for appending to a file. Server version much more complex """ 
+import os
 
 class RecordFileHandler(object):
     def __init__(self, file):
@@ -9,12 +10,12 @@ class RecordFileHandler(object):
 
     def record(self, text, truncationPoint=False):
         if self.file:
-            writeFile = open(self.file, "a")
             if truncationPoint:
-                self.lastTruncationPoint = writeFile.tell()
-                self.recordedSinceTruncationPoint = []
+                self.lastTruncationPoint = os.path.getsize(self.file)
+                self.recordedSinceTruncationPoint = []                
             if self.lastTruncationPoint is not None:
                 self.recordedSinceTruncationPoint.append(text)
+            writeFile = open(self.file, "a")
             writeFile.write(text)
             writeFile.flush()
             writeFile.close()
