eb5419cf7ec561e8d935425d6e46be35e0aef961
Geoff Bache <geoff.bache@pobox.com>
2022-03-24 16:42:58 +0100

    CRLF is the standard line terminator in HTTP requests, not platform dependent

--- capturemock/encodingutils.py
+++ capturemock/encodingutils.py
@@ -5,16 +5,10 @@ import os
 def decodeBytes(rawBytes):
     try:
         textStr = rawBytes.decode(getpreferredencoding()) if rawBytes else ""
-        if os.name == "nt":
-            return textStr.replace(os.linesep, "\n")
-        else:
-            return textStr
+        return textStr.replace("\r\n", "\n")
     except UnicodeDecodeError:
         return "0x" + rawBytes.hex()
 
 def encodeString(textStr):
     rawBytes = textStr.encode(getpreferredencoding())
-    if os.name == "nt":
-        return rawBytes.replace(b"\n", b"\r\n")
-    else:
-        return rawBytes
+    return rawBytes.replace(b"\n", b"\r\n")
