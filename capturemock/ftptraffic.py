889e0285524b4000fc5c910e2c8b80e42ef50d5a
Geoff Bache <geoff.bache@qualisys.se>
2025-04-14 13:35:21 +0200

    Fixing FTP support + adding replay

--- capturemock/ftptraffic.py
+++ capturemock/ftptraffic.py
@@ -1,25 +1,28 @@
 
 import ftplib
 from pprint import pformat
-from capturemock import traffic, encodingutils, clientservertraffic
-from datetime import datetime
+from capturemock import traffic, encodingutils, clientservertraffic, fileedittraffic
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
-import io, sys, logging, os
+import io, sys, os
 
 class FTPTrafficHandler(FTPHandler):
     dispatcher = None
     requestCount = 0
+    dummyFileName = "ftp_dummy.txt"
     authorizer = DummyAuthorizer()
     authorizer.add_user("capturemock", "capturemock", ".", perm='elradfmwMT')
     def pre_process_command(self, line, cmd, arg):
-        if self.username == "capturemock" or (cmd == "USER" and arg == "capturemock"):
-            FTPHandler.pre_process_command(self, line, cmd, arg)
-        else:
+        if os.path.isfile(self.dummyFileName):
+            os.remove(self.dummyFileName)
+        internal_cmd = self.username == "capturemock" or (cmd == "USER" and arg == "capturemock")
+        if not internal_cmd:
             FTPTrafficHandler.requestCount += 1
             traffic = FtpClientTraffic(cmd, arg, rcHandler=self.dispatcher.rcHandler, handler=self)
             self.dispatcher.process(traffic, self.requestCount)
+        if cmd != "LIST":
+            FTPHandler.pre_process_command(self, line, cmd, arg)
             
     def ftp_STOR(self, path, mode="w"):
         fn = os.path.basename(path)
@@ -33,9 +36,22 @@ class FTPTrafficHandler(FTPHandler):
             ftp.connect(host, int(port))
             clientservertraffic.ClientSocketTraffic.setServerLocation(servAddr, clientRecord=True)
             FtpClientTraffic.serverFtp = ftp
-            FTPHandler.ftp_STOR(self, fn, mode)
+            dummy = os.path.join(os.path.dirname(fn), self.dummyFileName)
+            FTPHandler.ftp_STOR(self, dummy, mode)
 
+    def is_success(self, response):
+        return response.startswith("2") or response.startswith("3")
+
+    def handle_cmd_response(self, cmd, arg, response):
+        if self.is_success(response):
+            self.dispatcher.diag.debug("handle response " + cmd + " " + arg + " " + response)
+            if cmd == "PASS":
+                ftp_dir = fileedittraffic.FileEditTraffic.replayFileEditDir or fileedittraffic.FileEditTraffic.recordFileEditDir
+                if not os.path.isdir(ftp_dir):
+                    os.makedirs(ftp_dir)
+                self.authorizer.add_user(self.username, arg, ftp_dir, perm='elradfmwMT')
     
+
 class FtpTrafficServer:
     @classmethod
     def createServer(cls, address, dispatcher):
@@ -53,7 +69,6 @@ class FtpTrafficServer:
         return self.host + ":" + str(values[1])
         
     def run(self):
-        logging.basicConfig(filename='/var/log/pyftpd.log', level=logging.INFO)
         self.server.serve_forever()
 
     @staticmethod
@@ -66,8 +81,12 @@ class FtpTrafficServer:
         with ftplib.FTP() as ftp:
             ftp.connect(host, int(port))
             ftp.login("capturemock", "capturemock")
-            with io.BytesIO(b"") as f:
-                ftp.storbinary("STOR CAPTUREMOCK_SHUTDOWN", f)
+            try:
+                with io.BytesIO(b"") as f:
+                    ftp.storbinary("STOR CAPTUREMOCK_SHUTDOWN", f)
+            except EOFError:
+                # We expect this, the server will shutdown and not respond to the request
+                pass
 
 class FtpServerStateTraffic(clientservertraffic.ServerStateTraffic):
     def __init__(self, dest, rcHandler):
@@ -90,27 +109,42 @@ class FtpClientTraffic(clientservertraffic.ClientSocketTraffic):
         if self.cmd == "LIST":
             lines = []
             response = self.serverFtp.retrlines(self.text, lambda l: lines.append(l))
-            return [ FtpListTraffic(lines, handler=self.handler), FtpServerTraffic(response, handler=self.handler)  ]
+            return [ FtpListTraffic(lines, handler=self.handler), FtpServerTraffic(self.cmd, self.arg, response, handler=self.handler)  ]
         elif self.cmd == "RETR":
             parts = []
-            response = self.serverFtp.retrbinary(self.text, lambda d: parts.append(d))
-            return [ FtpFileTraffic(self.arg, b"".join(parts), handler=self.handler), FtpServerTraffic(response, handler=self.handler)  ]
+            try:
+                response = self.serverFtp.retrbinary(self.text, lambda d: parts.append(d))
+                return [ FtpFileTraffic(self.arg, b"".join(parts)), FtpServerTraffic(self.cmd, self.arg, response, handler=self.handler)  ]
+            except ftplib.Error as e:
+                return [ FtpServerTraffic(self.cmd, self.arg, str(e), handler=self.handler) ]
         else:
             response = self.serverFtp.sendcmd(self.text)
-            return [ FtpServerTraffic(response, handler=self.handler) ]
+            return [ FtpServerTraffic(self.cmd, self.arg, response, handler=self.handler) ]
                     
     @classmethod
     def isClientClass(cls):
         return True
+    
+    def makeResponseTraffic(self, rawText, responseClass, rcHandler):
+        if responseClass is FtpServerTraffic:
+            return responseClass(self.cmd, self.arg, rawText, self.handler)
+        elif responseClass is FtpListTraffic:
+            return responseClass(eval(rawText), self.handler)
+        elif responseClass is FtpFileTraffic:
+            return responseClass(rawText)
+        else:
+            return super(FtpClientTraffic, self).makeResponseTraffic(rawText, responseClass, rcHandler)
 
 class FtpServerTraffic(clientservertraffic.ServerTraffic):
-    def __init__(self, response, handler):
+    def __init__(self, cmd, arg, response, handler):
         clientservertraffic.ServerTraffic.__init__(self, response, None)
+        self.cmd = cmd
+        self.arg = arg
         self.handler = handler
     
     def forwardToDestination(self):
         if self.handler:
-            self.handler.respond(self.text)
+            self.handler.handle_cmd_response(self.cmd, self.arg, self.text)
         return []
     
 class FtpListTraffic(clientservertraffic.ServerTraffic):
@@ -122,19 +156,20 @@ class FtpListTraffic(clientservertraffic.ServerTraffic):
     
     def forwardToDestination(self):
         if self.handler:
-            self.handler.push_dtp_data("\n".join(self.lines), cmd="LIST")
+            self.handler.push_dtp_data("\n".join(self.lines).encode(), cmd="LIST")
         return []
     
 class FtpFileTraffic(clientservertraffic.ServerTraffic):
-    typeId = "LST"
-    def __init__(self, fn, data, handler):
+    typeId = "FIL"
+    def __init__(self, fn, data=None):
         clientservertraffic.ServerTraffic.__init__(self, fn, None)
-        self.handler = handler
         self.fn = fn
         self.data = data
     
     def forwardToDestination(self):
-        if self.handler:
-            self.handler.push_dtp_data(self.data, cmd="RETR")
+        recordDir = fileedittraffic.FileEditTraffic.recordFileEditDir
+        if recordDir and self.data:
+            with open(os.path.join(recordDir, self.fn), "wb") as f:
+                f.write(self.data)
         return []
     
