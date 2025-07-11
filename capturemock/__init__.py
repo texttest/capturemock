3a60893f0186b0e8aaf1493bba43fae0b5c8c95b
Geoff Bache <geoff.bache@qualisys.se>
2025-07-11 09:55:22 +0200

    bump version

--- capturemock/__init__.py
+++ capturemock/__init__.py
@@ -12,7 +12,7 @@ from datetime import datetime
 import bisect
 from urllib.request import urlopen
 
-version = "2.6.3"
+version = "2.6.4"
 
 class CaptureMockManager:
     fileContents = "import capturemock; capturemock.interceptCommand()\n"
